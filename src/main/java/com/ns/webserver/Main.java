31c27647c95c94caae5fa9555085751fd06bf178
90S31D0N <noelschaller32@outlook.com>
2026-01-13 13:48:40 +0100

    Change Text

--- src/main/java/com/ns/webserver/Main.java
+++ src/main/java/com/ns/webserver/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test 1");
         System.out.println("Test 2");
+        System.out.println("Test 3");
     }
 }
