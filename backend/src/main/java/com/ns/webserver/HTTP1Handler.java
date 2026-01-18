278a6f006dc719d739237e2cdb9ea1b726ad1dd7
90S31D0N <noelschaller32@outlook.com>
2026-01-18 23:29:17 +0100

    Add frontend and update build config

---
+++ backend/src/main/java/com/ns/webserver/HTTP1Handler.java
@@ -0,0 +1,23 @@
+package com.ns.webserver;
+
+import tcpframework.AbstractHandler;
+import tcpframework.HTTPRequest;
+import tcpframework.HTTPRequestHandler;
+import tcpframework.HTTPResponseHandler;
+
+import java.net.Socket;
+
+public class HTTP1Handler extends AbstractHandler {
+    @Override
+    protected void runTask(Socket socket) {
+        try {
+            HTTPRequest request = HTTPRequestHandler.parseHTTPRequest(socket);
+            System.out.println(request);
+            HTTPResponseHandler responseHandlerLocal = new HTTPResponseHandler("/app/static");
+//            HTTPResponseHandler responseHandlerLocal = new HTTPResponseHandler("backend/src/main/resources/static");
+            responseHandlerLocal.handleRequest(request, socket);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ frontend/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  }
+};
+
+export default nextConfig;
