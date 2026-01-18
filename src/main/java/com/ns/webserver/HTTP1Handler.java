6680a58a8e10d84e4e2953e1ccafba581655c923
90S31D0N <noelschaller32@outlook.com>
2026-01-18 18:32:43 +0100

    add static file serving

--- src/main/java/com/ns/webserver/HTTP1Handler.java
+++ src/main/java/com/ns/webserver/HTTP1Handler.java
@@ -3,6 +3,8 @@ package com.ns.webserver;
 import tcpframework.AbstractHandler;
 import tcpframework.HTTPRequest;
 import tcpframework.HTTPRequestHandler;
+import tcpframework.HTTPResponseHandler;
+
 import java.net.Socket;
 
 public class HTTP1Handler extends AbstractHandler {
@@ -11,6 +13,8 @@ public class HTTP1Handler extends AbstractHandler {
         try {
             HTTPRequest request = HTTPRequestHandler.parseHTTPRequest(socket);
             System.out.println(request);
+            HTTPResponseHandler responseHandler = new HTTPResponseHandler("src/main/resources/static");
+            responseHandler.handleRequest(request, socket);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
---
+++ src/main/java/tcpframework/HTTPResponseHandler.java
@@ -0,0 +1,73 @@
+package tcpframework;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HTTPResponseHandler {
+
+    private final String rootPath;
+
+    public HTTPResponseHandler(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    public void handleRequest(HTTPRequest request, Socket socket) throws IOException {
+
+        switch (request.getMethod()){
+            case GET:
+                handleGetRequest(request, socket);
+                break;
+            case POST:
+                handlePostRequest(request, socket);
+                break;
+            default:
+                HTTPErrorHandler.sendNotImplemented(socket);
+                break;
+        }
+    }
+
+    private void handleGetRequest(HTTPRequest request, Socket socket) {
+        String path = request.getPath();
+        Path filePath = Paths.get(rootPath, path);
+
+        if (!Files.exists(filePath) || Files.isDirectory(filePath)) {
+            HTTPErrorHandler.sendNotFound(socket);
+            return;
+        }
+
+        try {
+            String body = Files.readString(filePath, StandardCharsets.UTF_8);
+            String mimeType = Files.probeContentType(filePath);
+            sendResponse(socket, mimeType, body);
+        } catch (IOException e) {
+            HTTPErrorHandler.sendInternalError(socket);
+        }
+
+
+    }
+
+    private void handlePostRequest(HTTPRequest request, Socket socket) {
+        HTTPErrorHandler.sendNotImplemented(socket);
+    }
+
+    private void sendResponse(Socket socket, String mimeType, String body) {
+        try {
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            writer.write(String.format("HTTP/1.1 %d %s\r\n", 200, "OK"));
+            writer.write(String.format("Content-Type: %s; charset=utf-8\r\n", mimeType));
+            writer.write("Connection: close\r\n\r\n");
+            writer.write(body);
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Failed to send response: " + e.getMessage());
+        }
+    }
+
+}
